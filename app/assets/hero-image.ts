f4ff2fe973ed87386af30475e2b4a2c7b0117782
Kaloyan Madzhunov <k.madzhunov@gmail.com>
2026-07-11 20:54:42 +0300

    Assemble DeskLife hero image

---
+++ app/assets/hero-image.ts
@@ -0,0 +1,7 @@
+import chunk0 from "./hero-chunk-0";
+import chunk1 from "./hero-chunk-1";
+import chunk2 from "./hero-chunk-2";
+import chunk3 from "./hero-chunk-3";
+import chunk4 from "./hero-chunk-4";
+
+export const heroImage = `data:image/webp;base64,${chunk0}${chunk1}${chunk2}${chunk3}${chunk4}`;
